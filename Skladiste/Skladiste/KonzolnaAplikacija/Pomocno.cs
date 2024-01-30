ee3ca3ddb47be81a1964db2d7012da66c48753dd
Roman <149431633+Rokiart@users.noreply.github.com>
2024-01-30 20:34:20 +0100

    priprema konzolne aplikacije

---
+++ Skladiste/Skladiste/KonzolnaAplikacija/Pomocno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skladiste.KonzolnaAplikacija
+{
+    internal class Pomocno
+    {
+        public static string UcitajString(string poruka)
+        {
+            string s;
+            while (true)
+            {
+                Console.WriteLine(poruka);
+                s = Console.ReadLine();
+                if(s.Trim().Length == 0)
+                {
+                    Console.WriteLine("Obavezan unos");
+                    continue;
+                }
+                return s;
+
+            }
+
+        }
+    }
+}

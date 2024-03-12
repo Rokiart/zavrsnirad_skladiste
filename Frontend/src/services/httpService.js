3358628492621c219e21157f18d739387e20026c
Roman <149431633+Rokiart@users.noreply.github.com>
2024-03-12 17:53:20 +0100

    Delete Frontend directory

--- Frontend/src/services/httpService.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const httpService = axios.create({
-    baseURL: 'https://romanzaric-001-site1.itempurl.com/',
-    headers:{
-        'Content-Type' : 'application/json'
-    }
-});

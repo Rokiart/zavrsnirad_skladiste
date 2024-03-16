b351ac5f44b1f3fea42154ef7b929ec6ee728ffc
Roman <149431633+Rokiart@users.noreply.github.com>
2024-03-16 14:27:19 +0100

    zavrsen backand

--- SKladisteAppl/Frontend/src/services/httpService.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const httpService = axios.create({
-    baseURL: 'https://romanzaric-001-site1.itempurl.com/api/v1',
-    headers:{
-        'Content-Type' : 'application/json'
-    }
-});
